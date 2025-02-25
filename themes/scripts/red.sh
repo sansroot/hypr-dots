d8af5e7e262a8902e5d3a48767dd55d36fdba99f
sansroot <sansroot@github.com>
2025-02-25 15:43:29 -0300

    theme addition

--- themes/scripts/red.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp -r ~/Documents/themes/themes/red/* -t ~/.config/
-swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
-hyprctl setcursor hypr-dots 24
-killall waybar
-waybar
