fee7e3867a4cf7befe6bff6465716a77620145d5
sansroot <sansroot@github.com>
2025-03-12 23:53:28 -0300

    customized cursor addition

--- themes/scripts/catppuccin/sapphire.sh
+++ themes/scripts/catppuccin/sapphire.sh
@@ -2,7 +2,7 @@
 
 cp -r ~/Documents/themes/themes/catppuccin/sapphire/* -t ~/.config/
 swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
-hyprctl setcursor hypr-dots 24
+hyprctl setcursor hypr-dots-sapphire 24
 gsettings set org.gnome.desktop.interface cursor-theme 'hypr-dots-sapphire' &> /dev/null
 killall waybar
 waybar
