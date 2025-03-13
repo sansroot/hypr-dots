b01283989086e4f878ffcd4f10534f8e12b147ca
San's <fsanches2002@outlook.com>
2025-03-12 23:45:17 -0300

    Update dotfiles_installer.sh

--- dotfiles_installer.sh
+++ dotfiles_installer.sh
@@ -331,11 +331,11 @@ sudo chmod -R 777 /usr/share/themes
 sudo chmod -R 777 /usr/share/icons
 sudo chmod -R 777 /usr/bin/papirus-folders
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' &> /dev/null
-gsettings set org.gnome.desktop.interface gtk-theme "hypr-dots-mauve" &> /dev/null
+gsettings set org.gnome.desktop.interface gtk-theme "hypr-dots-red" &> /dev/null
 
 gsettings set org.gnome.desktop.interface cursor-theme 'hypr-dots-red' &> /dev/null
 
-papirus-folders -C cat-mocha-mauve &> /dev/null
+papirus-folders -C cat-mocha-red &> /dev/null
 
 clear
 echo "Now, let's load the Hyprland, Waybar, and Rofi themes into the theme-switcher variable for the first time."
