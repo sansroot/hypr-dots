d4ae12c7ef61ef62ab8c1f9456d4ee025243bae3
sansroot <sansroot@github.com>
2025-03-12 23:38:43 -0300

    customized cursor addition

--- themes/scripts/catppuccin/lavender.sh
+++ themes/scripts/catppuccin/lavender.sh
@@ -3,5 +3,6 @@
 cp -r ~/Documents/themes/themes/catppuccin/lavender/* -t ~/.config/
 swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
 hyprctl setcursor hypr-dots 24
+gsettings set org.gnome.desktop.interface cursor-theme 'hypr-dots-lavender' &> /dev/null
 killall waybar
 waybar
