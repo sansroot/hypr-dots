7e2c5a894b3250dfd8bfb2095016557b64be7aad
sansroot <sansroot@github.com>
2025-03-12 23:53:24 -0300

    customized cursor addition

--- themes/scripts/catppuccin/blue.sh
+++ themes/scripts/catppuccin/blue.sh
@@ -2,7 +2,7 @@
 
 cp -r ~/Documents/themes/themes/catppuccin/blue/* -t ~/.config/
 swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
-hyprctl setcursor hypr-dots 24
+hyprctl setcursor hypr-dots-blue 24
 gsettings set org.gnome.desktop.interface cursor-theme 'hypr-dots-blue' &> /dev/null
 killall waybar
 waybar
--- themes/scripts/monochrome/black.sh
+++ themes/scripts/monochrome/black.sh
@@ -2,7 +2,7 @@
 
 cp -r ~/Documents/themes/themes/monochrome/black/* -t ~/.config/
 swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
-hyprctl setcursor hypr-dots 24
+hyprctl setcursor hypr-dots-black 24
 gsettings set org.gnome.desktop.interface cursor-theme 'hypr-dots-black' &> /dev/null
 killall waybar
 waybar
