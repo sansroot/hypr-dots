c236bb2bf69e4bfb645fb8f5f797e9bd8980daf7
sansroot <sansroot@github.com>
2025-03-12 23:39:18 -0300

    customized cursor addition

--- themes/scripts/monochrome/black.sh
+++ themes/scripts/monochrome/black.sh
@@ -3,5 +3,6 @@
 cp -r ~/Documents/themes/themes/monochrome/black/* -t ~/.config/
 swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
 hyprctl setcursor hypr-dots 24
+gsettings set org.gnome.desktop.interface cursor-theme 'hypr-dots-black' &> /dev/null
 killall waybar
 waybar
