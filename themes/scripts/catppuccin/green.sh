1aaaf8e95ed712b191cd47e1f90a7092015a24ef
sansroot <sansroot@github.com>
2025-03-12 23:55:03 -0300

    customized cursor addition

--- themes/scripts/catppuccin/green.sh
+++ themes/scripts/catppuccin/green.sh
@@ -2,7 +2,7 @@
 
 cp -r ~/Documents/themes/themes/catppuccin/green/* -t ~/.config/
 swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
-hyprctl setcursor hypr-dots 24
+hyprctl setcursor hypr-dots-green 24
 gsettings set org.gnome.desktop.interface cursor-theme 'hypr-dots-green' &> /dev/null
 killall waybar
 waybar
--- themes/scripts/catppuccin/yellow.sh
+++ themes/scripts/catppuccin/yellow.sh
@@ -2,7 +2,7 @@
 
 cp -r ~/Documents/themes/themes/catppuccin/yellow/* -t ~/.config/
 swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
-hyprctl setcursor hypr-dots 24
+hyprctl setcursor hypr-dots-yellow 24
 gsettings set org.gnome.desktop.interface cursor-theme 'hypr-dots-yellow' &> /dev/null
 killall waybar
 waybar
