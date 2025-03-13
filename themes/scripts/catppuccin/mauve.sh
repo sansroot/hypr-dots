788816efdc1719f49ef0a364f7b316006a7ab54e
sansroot <sansroot@github.com>
2025-03-12 23:53:14 -0300

    customized cursor addition

--- themes/scripts/catppuccin/mauve.sh
+++ themes/scripts/catppuccin/mauve.sh
@@ -2,7 +2,7 @@
 
 cp -r ~/Documents/themes/themes/catppuccin/mauve/* -t ~/.config/
 swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
-hyprctl setcursor hypr-dots 24
+hyprctl setcursor hypr-dots-mauve 24
 gsettings set org.gnome.desktop.interface cursor-theme 'hypr-dots-mauve' &> /dev/null
 killall waybar
 waybar
