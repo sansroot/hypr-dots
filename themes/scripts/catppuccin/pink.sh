3c886dfe05af3a0f6a33be46e5d0666e7e8153e8
sansroot <sansroot@github.com>
2025-03-12 23:52:59 -0300

    customized cursor addition

--- themes/scripts/catppuccin/pink.sh
+++ themes/scripts/catppuccin/pink.sh
@@ -2,7 +2,7 @@
 
 cp -r ~/Documents/themes/themes/catppuccin/pink/* -t ~/.config/
 swww img ~/.config/hypr/wallpaper.jpg --transition-type wipe --transition-fps 240
-hyprctl setcursor hypr-dots 24
+hyprctl setcursor hypr-dots-pink 24
 gsettings set org.gnome.desktop.interface cursor-theme 'hypr-dots-pink' &> /dev/null
 killall waybar
 waybar
