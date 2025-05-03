a6cdfcbf59d7d367a4923d4ecca867dfc4fe189d
sansroot <sansroot@github.com>
2025-05-02 22:00:57 -0300

    theme addition

--- themes/theme-switcher.sh
+++ themes/theme-switcher.sh
@@ -7,16 +7,28 @@ if [ -z "$selected" ]; then
 fi
 
 
+if [[ "$selected" == *"misc"* ]]; then
+    selected2=$(ls ~/Documents/themes/scripts/$selected/ | sed 's/\.sh$//' | rofi -dmenu -p "HYPRLAND - WAYBAR - ROFI" -theme ~/Documents/themes/theme-switcher-misc.rasi) && bash ~/Documents/themes/scripts/$selected/$selected2.sh
+    if [ -z "$selected2" ]; then
+        killall rofi
+        exit 0
+    fi
+    selected1=$(ls ~/Documents/themes/icon_theme_scripts/$selected/ | sed 's/\.sh$//' | rofi -dmenu -p "GTK THEME - ICONS" -theme ~/Documents/themes/theme-switcher1-misc.rasi) && bash ~/Documents/themes/icon_theme_scripts/$selected/$selected1.sh
+    if [ -z "$selected1" ]; then
+        killall rofi
+        exit 0
+    fi
 
-selected2=$(ls ~/Documents/themes/scripts/$selected/ | sed 's/\.sh$//' | rofi -dmenu -p "HYPRLAND - WAYBAR - ROFI" -theme ~/Documents/themes/theme-switcher.rasi) && bash ~/Documents/themes/scripts/$selected/$selected2.sh
-if [ -z "$selected2" ]; then
-    killall rofi
-    exit 0
-fi
+else
+    selected2=$(ls ~/Documents/themes/scripts/$selected/ | sed 's/\.sh$//' | rofi -dmenu -p "HYPRLAND - WAYBAR - ROFI" -theme ~/Documents/themes/theme-switcher.rasi) && bash ~/Documents/themes/scripts/$selected/$selected2.sh
+    if [ -z "$selected2" ]; then
+        killall rofi
+        exit 0
+    fi
+    selected1=$(ls ~/Documents/themes/icon_theme_scripts/$selected/ | sed 's/\.sh$//' | rofi -dmenu -p "GTK THEME - ICONS" -theme ~/Documents/themes/theme-switcher1.rasi) && bash ~/Documents/themes/icon_theme_scripts/$selected/$selected1.sh
+    if [ -z "$selected1" ]; then
+        killall rofi
+        exit 0
+    fi
 
-
-selected1=$(ls ~/Documents/themes/icon_theme_scripts/$selected/ | sed 's/\.sh$//' | rofi -dmenu -p "GTK THEME - ICONS" -theme ~/Documents/themes/theme-switcher1.rasi) && bash ~/Documents/themes/icon_theme_scripts/$selected/$selected1.sh
-if [ -z "$selected1" ]; then
-    killall rofi
-    exit 0
 fi
