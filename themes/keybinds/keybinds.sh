a7835de4478602c39b9f37d87a68a445c6606e83
sansroot <sansroot@github.com>
2025-05-04 19:27:58 -0300

    theme addition

--- themes/keybinds/keybinds.sh
+++ themes/keybinds/keybinds.sh
@@ -6,4 +6,4 @@ keybinds="$HOME/Documents/themes/keybinds/keybinds.txt"
 keybinds1=$(cat "$keybinds")
 
 # Usa o rofi para exibir as keybinds
-echo "$keybinds1" | rofi -dmenu -p "Keybinds"
+echo "$keybinds1" | rofi -dmenu -theme ~/Documents/themes/keybinds/keybinds.rasi -p "Keybinds"
