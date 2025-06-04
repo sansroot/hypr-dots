c2fdc9a2aba37824c531ae430d7ce5322c6ad519
sansroot <sansroot@github.com>
2025-06-04 01:36:02 -0300

    Replace 'swww init' with 'swww-daemon' after the update

--- dotfiles_installer.sh
+++ dotfiles_installer.sh
@@ -528,7 +528,7 @@ sudo cp -r ./cursor/hypr-dots* -t /usr/share/icons/
 
 sudo cp -r ./gtkthemes/* -t /usr/share/themes/
 cp -r ./.zshrc ./.p10k.zsh -t ~/
-swww init &> /dev/null
+swww-daemon &> /dev/null
 swww img ~/.config/hypr/wallpaper.jpg &> /dev/null
 killall waybar &> /dev/null
 waybar &> /dev/null &
